66f8c913dab9088cfa840197863bcda1c455e2cc
robertenes <enescanbal@hotmail.com>
2020-10-22 18:55:52 +0300

    Upload Project First

---
+++ INSERT-PROCEDURE-WEBABI/Controllers/EmployeeAPIController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using INSERT_PROCEDURE_WEBABI.Models;
+namespace INSERT_PROCEDURE_WEBABI.Controllers
+{
+    public class EmployeeAPIController : ApiController
+    {
+        EmployeeEntity context = new EmployeeEntity();
+        public IHttpActionResult GetEmployee()
+        {
+            var model = context.PROC_EMPLOYEE_SELECT();
+            return Ok(model);
+        }
+        [HttpPost]
+        public IHttpActionResult PostEmployee(TABLE_EMPLOYEE emp)
+        {
+            if (ModelState.IsValid)
+            {
+                context.PROC_EMP(emp.EMPLOYEENAME, emp.EMPLOYEESURNAME, emp.EMPLOYEEAGE, emp.EMPLOYEEMAIL);
+                context.SaveChanges();
+            }
+            return Ok();
+        }
+
+    }
+}
--- INSERT-PROCEDURE-WEBABI/Controllers/HomeController.cs
+++ INSERT-PROCEDURE-WEBABI/Controllers/HomeController.cs
@@ -1,17 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
-
+using INSERT_PROCEDURE_WEBABI.Models;
 namespace INSERT_PROCEDURE_WEBABI.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            IEnumerable<TABLE_EMPLOYEE> model = null;
+            HttpClient ht = new HttpClient();
+            ht.BaseAddress = new Uri("http://localhost:64918/api/EmployeeAPI");
 
+            var record = ht.GetAsync("EmployeeAPI");
+            record.Wait();
+
+            var readData = record.Result;
+            if(readData.IsSuccessStatusCode)
+            {
+                var displayResult = readData.Content.ReadAsAsync<IEnumerable<TABLE_EMPLOYEE>>();
+                displayResult.Wait();
+                model = displayResult.Result;
+            }
+        
+            return View(model);
+        }
+        public ActionResult CreateEmployee()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateEmployee(TABLE_EMPLOYEE emp)
+        {
+            if(ModelState.IsValid)
+            {
+                HttpClient ht = new HttpClient();
+                ht.BaseAddress = new Uri("http://localhost:64918/api/EmployeeAPI");
+
+                var insertPort = ht.PostAsJsonAsync<TABLE_EMPLOYEE>("EmployeeAPI", emp);
+                insertPort.Wait();
+                var savePort = insertPort.Result;
+                if(savePort.IsSuccessStatusCode)
+                {
+                    ViewBag.Messagge = emp.EMPLOYEENAME + " is success.";
+                }
+            }
             return View();
         }
     }
